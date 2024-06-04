36450ffa1bb7f9b1ed230bcb8f403c57875f61dd
Faizsamjani999 <faizsamjani999@gmail.com>
2024-06-04 14:58:03 +0530

    To-Do List-2

---
+++ day2/todolist2/src/Components/Todo.jsx
@@ -0,0 +1,23 @@
+
+
+function Todo()
+{
+
+
+
+
+    return(
+        <div class="container">
+    <h1>todo</h1>
+    <form id="listForm">
+      <input type="text" id="listInput" />
+      <button id="listBtn">Add List</button>
+    </form>
+    <ul>
+    </ul>
+  </div>
+    )
+
+}
+
+export default Todo
