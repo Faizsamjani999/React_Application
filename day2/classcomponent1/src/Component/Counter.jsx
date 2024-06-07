7e46aefc86aeb0e4fa146bf909b8d8d24e257882
Faizsamjani999 <faizsamjani999@gmail.com>
2024-06-07 17:01:25 +0530

    changes

--- day2/classcomponent1/src/Component/Counter.jsx
+++ day2/classcomponent1/src/Component/Counter.jsx
@@ -44,6 +44,20 @@ class Counter extends Component{
                     <button className="btn btn-success" onClick={this.decreament} id="dec"><i class="bi bi-dash-circle"></i></button>
                     <button className="btn btn-danger" onClick={this.reset}><i class="bi bi-backspace"></i></button>
                 </div>
+                <div id="write">
+                <div>
+                    <h1>Class Component</h1>
+                    <p>A class component requires you to extend from React. Component and create a render function that returns a React element.</p>
+                    <p>It must have the render() method returning JSX which is syntactically similar to HTML</p>
+                    <p>The class component is instantiated and different life cycle method is kept alive and is run and invoked depending on the phase of the class component.</p>
+                </div>
+                <div>
+                    <h1>Functional Component</h1>
+                    <p>A functional component is just a plain JavaScript pure function that accepts props as an argument and returns a React element(JSX).</p>
+                    <p>There is no render method used in functional components.</p>
+                    <p>Functional components run from top to bottom and once the function is returned it can’t be kept alive.</p>
+                </div>
+                </div>
             </div>
         )
     }
